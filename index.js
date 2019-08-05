abd7dc13f5a7141906cfb53ea91ab9ce9ca4942e
kishore2486 <ramkishore157359@gmail.com>
2019-08-05 09:22:06 +0530

    created middleware using multer

---
+++ index.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+
+module.exports = (dest, fileName, type) => {
+
+    let arr = [];
+
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, dest);
+        },
+        filename: function (req, file, cb) {
+            arr.push(dest.splice(1) + new Date() + file.originalname);
+            req.paths = arr;
+            cb(null, new Date() + file.originalname);
+        }
+    });
+
+    if (type === 'single') {
+        return multer({ storage: storage }).single(fileName);
+    } else if (type === 'multiple') {
+        return multer({ storage: storage }).array(fileName);
+    } else {
+        return multer().none();
+    }
+
+}
