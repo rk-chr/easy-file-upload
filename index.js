7c7a913692b9506fab0e84e559e93c65d21696f2
kishore2486 <ramkishore157359@gmail.com>
2019-11-06 18:41:00 +0530

    changed filename

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const multer = require('multer');
+const multer = require("multer");
 
 module.exports = (dest, fileName, type) => {
-    let arr = [];
+  let arr = [];
 
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, dest);
-        },
-        filename: function (req, file, cb) {
-            arr.push(new Date().getTime() + file.originalname);
-            req.paths = arr;
-            cb(null, new Date() + file.originalname);
-        }
-    });
-
-    if (type === 'single') {
-        return multer({ storage: storage }).single(fileName);
-    } else if (type === 'multiple') {
-        return multer({ storage: storage }).array(fileName);
-    } else {
-        return multer().none();
+  const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, dest);
+    },
+    filename: function(req, file, cb) {
+      arr.push(new Date().toISOString() + file.originalname);
+      req.paths = arr;
+      cb(null, new Date().toISOString() + file.originalname);
     }
-}
+  });
+
+  if (type === "single") {
+    return multer({ storage: storage }).single(fileName);
+  } else if (type === "multiple") {
+    return multer({ storage: storage }).array(fileName);
+  } else {
+    return multer().none();
+  }
+};
